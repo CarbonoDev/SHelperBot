75e28756b44575bda601270141f095f9c6994e55
Marco Rivadeneyra <marcorivm@gmail.com>
2016-12-07 21:42:41 -0600

    Support <b><i><code><pre> tags, all without attributes, still missing support for links

--- app/Telegram/Transformers/SafeHTMLTransformer.php
+++ app/Telegram/Transformers/SafeHTMLTransformer.php
@@ -9,11 +9,23 @@ class SafeHTMLTransformer
 
 	public static function transform($unsafe_html)
 	{
-		$with_spaces = str_replace('&nbsp;', ' ', $unsafe_html);
-		$decoded_html = html_entity_decode($with_spaces);
-		$clean_html = strip_tags($decoded_html, '<a><b><i><code><pre>');
-		$encoded_html = htmlentities($clean_html);
-		$safe_html = str_replace(['<','>','&lt;b&gt;','&lt;/b&gt;'], ['&lt;','&gt;','<b>','</b>'], $encoded_html);
+		$safe_html = $unsafe_html;
+		// $safe_html = str_replace('&nbsp;', ' ', $unsafe_html);
+		// $safe_html = static::entities_to_unicode($safe_html);
+		$safe_html = html_entity_decode($safe_html, ENT_QUOTES, 'UTF-8');
+		$safe_html = strip_tags($safe_html, '<a><b><i><code><pre>');
+		$safe_html = htmlspecialchars($safe_html, ENT_QUOTES, 'UTF-8');
+		$safe_html = str_replace([
+			'&lt;b&gt;','&lt;/b&gt;',
+			'&lt;i&gt;','&lt;/i&gt;',
+			'&lt;code&gt;','&lt;/code&gt;',
+			'&lt;pre&gt;','&lt;/pre&gt;',
+			], [
+			'<b>','</b>',
+			'<i>','</i>',
+			'<code>','</code>',
+			'<pre>','</pre>',
+			], $safe_html);
 		return  $safe_html;
 	}
 }
