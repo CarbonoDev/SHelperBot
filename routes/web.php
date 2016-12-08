bb1ca0c4264850c13a94f1d14663bd64edbca68c
Marco Rivadeneyra <marcorivm@gmail.com>
2016-12-08 16:39:04 -0600

    fix webhook
    Signed-off-by: Marco Rivadeneyra <marcorivm@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -51,89 +51,88 @@ Route::get('/getMessages', function () {
 
 // Example of POST Route:
 Route::post('/P31ZDkeJZU2UmsWoI7/webhook', function () {
-	$updates = collect(Telegram::commandsHandler(true));
+	$update = Telegram::commandsHandler(true));
 
-	$updates->each(function($update) {
-		if($update->isType('inline_query')) {
-			$inline_query = $update->getInlineQuery();
-			$options = [];
+	$message = $update->getMessage();
+	if($message->isType('inline_query')) {
+		$inline_query = $message->getInlineQuery();
+		$options = [];
 
-			$query = $inline_query->getQuery();
-			$query_type = substr($query, 0, 1);
-			if(in_array($query_type, ['g', 'i', 'l', 'p', 'pi'])) {
-				$query = substr($query, 1);
-			} else {
-				$query_type = 'g';
-			}
+		$query = $inline_query->getQuery();
+		$query_type = substr($query, 0, 1);
+		if(in_array($query_type, ['g', 'i', 'l', 'p', 'pi'])) {
+			$query = substr($query, 1);
+		} else {
+			$query_type = 'g';
+		}
 
-			switch ($query_type) {
-				case 'i':
-					$options['search_type'] = 'image';
-					break;
+		switch ($query_type) {
+			case 'i':
+				$options['search_type'] = 'image';
+				break;
 
-				case 'l':
-					$options['siteSearch'] = 'https://laravel.com/docs';
-					break;
+			case 'l':
+				$options['siteSearch'] = 'https://laravel.com/docs';
+				break;
 
-				case 'p':
-					$options['siteSearch'] = 'http://php.net/';
-					break;
+			case 'p':
+				$options['siteSearch'] = 'http://php.net/';
+				break;
 
-				case 'pi':
-					$options['search_type'] = 'image';
-					$options['rights'] = 'cc_publicdomain';
-					break;
+			case 'pi':
+				$options['search_type'] = 'image';
+				$options['rights'] = 'cc_publicdomain';
+				break;
 
-				default:
-					# code...
-					break;
-			}
+			default:
+				# code...
+				break;
+		}
 
-			$search_response = GoogleCSE::search($query, 1, 10, $options);
-			if($search_response->total_results <= 0) {
-				return;
-			}
-			$results = collect($search_response->results);
+		$search_response = GoogleCSE::search($query, 1, 10, $options);
+		if($search_response->total_results <= 0) {
+			return;
+		}
+		$results = collect($search_response->results);
 
-			if($query_type == 'i' || $query_type == 'pi') {
+		if($query_type == 'i' || $query_type == 'pi') {
 
-				$telegram_results = $results->map(function($result) {
-					$inline_query_result = new \Telegram\Bot\Objects\InlineQuery\InlineQueryResultPhoto([
+			$telegram_results = $results->map(function($result) {
+				$inline_query_result = new \Telegram\Bot\Objects\InlineQuery\InlineQueryResultPhoto([
 
-						'id'        => gmp_strval(gmp_init(substr(md5($result->link), 0, 16), 16), 10),
-						'title'     => $result->title,
-						'photo_url' => $result->link,
-						'thumb_url' => $result->link,
-						'caption'   => $result->title,
-						'description'   => $result->snippet,
-					]);
-					return $inline_query_result;
-				});
-			} else {
+					'id'        => gmp_strval(gmp_init(substr(md5($result->link), 0, 16), 16), 10),
+					'title'     => $result->title,
+					'photo_url' => $result->link,
+					'thumb_url' => $result->link,
+					'caption'   => $result->title,
+					'description'   => $result->snippet,
+				]);
+				return $inline_query_result;
+			});
+		} else {
 
-				$telegram_results = $results->map(function($result) {
-					$inline_query_result = new \Telegram\Bot\Objects\InlineQuery\InlineQueryResultArticle([
-						'id'                       => get64BitHash($result->link),
-						'title'                    => $result->title,
-						'url'                      => $result->link,
-						'description'              => $result->snippet,
-						'input_message_content'    => new \Telegram\Bot\Objects\InputContent\InputTextMessageContent([
-							"parse_mode" => "html",
-							"message_text" => \App\Telegram\Transformers\SearchResultTransformer::transform($result)
-							])
-					]);
-					return $inline_query_result;
-				});
-
-			}
-
-
-			return Telegram::answerInlineQuery([
-				'inline_query_id' => $inline_query->getId(),
-				'results'=> json_encode($telegram_results),
-			]);
+			$telegram_results = $results->map(function($result) {
+				$inline_query_result = new \Telegram\Bot\Objects\InlineQuery\InlineQueryResultArticle([
+					'id'                       => get64BitHash($result->link),
+					'title'                    => $result->title,
+					'url'                      => $result->link,
+					'description'              => $result->snippet,
+					'input_message_content'    => new \Telegram\Bot\Objects\InputContent\InputTextMessageContent([
+						"parse_mode" => "html",
+						"message_text" => \App\Telegram\Transformers\SearchResultTransformer::transform($result)
+						])
+				]);
+				return $inline_query_result;
+			});
 
 		}
-	});
+
+
+		return Telegram::answerInlineQuery([
+			'inline_query_id' => $inline_query->getId(),
+			'results'=> json_encode($telegram_results),
+		]);
+
+	}
 	return 'ok';
 });
